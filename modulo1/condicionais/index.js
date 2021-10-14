89504517be381ce93b1046d475f064b6f54a2bb4
thaisa santos <vianatayviana@outlook.com>
2021-10-13 21:07:58 -0300

    condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,77 @@
+//const respostaDoUsuario = prompt("Digite o número que você quer testar")
+//const numero = Number(respostaDoUsuario)
+
+//if (numero % 2 === 0) {
+  //console.log("Passou no teste.")
+// else {
+  //console.log("Não passou no teste.")
+//}
+
+// 1 Questao interpretaçao de codigo
+// a. pede numeros inteiros
+// b. numeros pares
+// c. numeros impar
+
+// 2 questao interpretaçao de codigo
+// a. da o preço da fruta
+// b. 5,00
+// c. continua o valor 5 
+
+// 3 Questao interpretaçao de codigo
+
+// a.pergunta para o usuario  um numero
+// b.passou no teste, da erro
+// c. havera um valor indefenido
+
+// 1 escrita de codigo
+
+const idadeUsuario = prompt("Qual a sua idade")
+if(idadeUsuario >=18) {
+    console.log("voce pode dirigir");
+}else{
+    console.log("voce não pode dirigir");}
+
+// 2 escrita de codigo
+
+const turno = prompt("qual turno voce estudar")
+if (turno == "M"){
+    console.log("bom dia"); 
+}
+ else if  (turno == "V")
+ {
+    console.log("boa tarde");
+}
+ else (turno == "N") 
+{ 
+    console.log("boa noite");
+}
+
+// 3 escrita de codigo
+
+switch (turno) {
+    case 'M':
+        console.log('bom dia'); 
+        break;
+    case "V":
+        console.log("boa tarde");
+         break;
+    case "N" :
+        console.log('boa noite');
+        break;
+    default:
+        console.log('turno nao exitente')
+        
+}
+
+// 4
+
+const amigo = prompt("Qual genero do filme ?")
+const valor = prompt(" valor do ingresso ?")
+
+if (genero == fantasia);{
+console.log("bom filme")
+}
+
+
+
+
